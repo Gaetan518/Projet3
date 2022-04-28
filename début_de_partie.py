4b61942667970b078357001d58371cb1fd1e4ec7
Gaetan518 <96253862+Gaetan518@users.noreply.github.com>
2022-04-28 17:44:25 +0200

    Update début_de_partie.py

--- début_de_partie.py
+++ début_de_partie.py
@@ -7,3 +7,11 @@ def mot_hasard(t1):
     mot = random.choice(t1)
     return mot
 mot_hasard(t1)
+
+def nombre_lettre(mot):
+    nb_lettre = 0
+    for lettre in len(mot):
+        if lettre in mot:
+            nb_lettre += 1
+            print("Le mot choisi contient",nb_lettre,"lettres.")
+nombre_lettre(mot)
