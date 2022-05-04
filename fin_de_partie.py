ab8237b88b1d9cdcae035fb8b84c0eb3c7d61edf
Alchch1810 <96254377+Alchch1810@users.noreply.github.com>
2022-05-04 11:12:40 +0200

    Update fin_de_partie.py

--- fin_de_partie.py
+++ fin_de_partie.py
@@ -13,4 +13,23 @@ def fin_de_partie(lettre):
             return lose
         if compteur > 0:  #si mot trouvé avant que compteur soit = a 0 , point += compteur d'aessai restant
                 compteur += points
+                
+                
         
+import csv 
+liste_mot = open("liste_mot.csv") 
+t1 = list(csv.DictReader(liste_mot, delimiter = ";")) 
+
+import random
+def mot_hasard(t1): 
+    mot = random.choice(t1)
+    return mot
+mot_hasard(t1)
+
+def nombre_lettre(mot):
+    nb_lettre = 0
+    for lettre in len(mot):
+        if lettre in mot:
+            nb_lettre += 1
+            print("Le mot choisi contient",nb_lettre,"lettres.")
+nombre_lettre(mot)
