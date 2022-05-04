999bab0a7c92b2530961545b2874082d5cd7ff8c
Alchch1810 <96254377+Alchch1810@users.noreply.github.com>
2022-05-04 11:43:49 +0200

    Update fin_de_partie.py

--- fin_de_partie.py
+++ fin_de_partie.py
@@ -1,16 +1,12 @@
-def fin_de_partie(lettre):
-    win ="Tu as Gagné"
-    lose = "Tu as Perdu"
+def resultat(mot, lettre):
     compteur = 6
     points = 0
     
     for i in range(6):
         if lettre != mot:
-            compteur - 1 #si toute les lettres sont égale a mots, joueur gangne sinon deduire 1 de compteur
+            compteur - 1 #si toute les lettres = mots, joueur gangne sinon -1 au compteur
         else:
-            return win
-        if compteur == 0: #si compteur d'essai = 0, joueur a perdu
-            return lose
-        if compteur > 0:  #si mot trouvé avant que compteur soit = a 0 , point += compteur d'aessai restant
+            return True
+        if compteur > 0:  #si mot trouvé avant que compteur soit = a 0 , point += compteur d'essai restant
                 compteur += points
-                
+                return points
