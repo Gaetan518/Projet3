22286b743f25b5c85cd9d88cc291a928d8268a7d
Gaetan518 <96253862+Gaetan518@users.noreply.github.com>
2022-05-04 11:19:22 +0200

    Update partie_motus.py

--- partie_motus.py
+++ partie_motus.py
@@ -6,12 +6,12 @@ def fin_de_partie(lettre):
     
     for i in range(6):
         if lettre != mot:
-            compteur - 1 #si toute les lettres sont égale a mots, joueur gangne sinon deduire 1 de compteur
+            compteur - 1 #si toutes les lettres sont égales a mots, joueur gagne sinon deduire 1 de compteur
         else:
             return win
         if compteur == 0: #si compteur d'essai = 0, joueur a perdu
             return lose
-        if compteur > 0:  #si mot trouvé avant que compteur soit = a 0 , point += compteur d'aessai restant
+        if compteur > 0:  #si mot trouvé avant que compteur soit = a 0 , point += compteur d'essai restant
                 compteur += points
                 
                 
