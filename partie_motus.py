e52700a98d2daf8472a95f8a81d708289dfc23dc
Alchch1810 <96254377+Alchch1810@users.noreply.github.com>
2022-05-05 17:40:16 +0200

    Update partie_motus.py

--- partie_motus.py
+++ partie_motus.py
@@ -1,18 +1,42 @@
 import csv 
 import random
+compteur = 6
 
 def resultat(mot, lettre):
     compteur = 6
     points = 0
+
     
-    for i in range(6):
-        if lettre != mot:
-            compteur - 1 #si toute les lettres = mots, joueur gangne sinon -1 au compteur
-        else:
-            return True
-        if compteur > 0:  #si mot trouvé avant que compteur soit = a 0 , point += compteur d'essai restant
-                compteur += points
-                return points
+def win(lettre, mot):
+    """Defini si le joueur a placer toute les lettre du mot choisidans la bonne place et 
+    que le compteur ne soit pas = 0 , le joueur gagne et retourne un texte. """
+    win = "Tu as gagné"
+    if lettre == mot:
+        return win
+    
+
+    def lose(lettre, mots):
+    """Defini si le joueur n'a pas placer toutes les lettre du mot dans la bonne place 
+    mais que le compteur est = 0 , le joueur perd et retourne un texte. """
+    lose = "tu as perdue"
+    if compteur == 0:
+        return lose
+
+    
+def retry(lettre, mot):
+    """Si le lettre du mot ne sont pas toute dans la bonne place mais que 
+    le compteur n'est pas a 0, le joueur a un nouvelle essai de deviner. """
+    if lettre != mots:
+        compteur - 1
+ 
+
+def point(compteur):
+    """Quand le joueur a ganger, des points sont comptabiliser 
+    en fonction du nombre d'essai que le joueur a du utiliser. """
+    points = 0
+    if compteur >= 1:
+        points = compteur
+
                 
 def mot_hasard(t1): 
     mot = random.choice(t1)
