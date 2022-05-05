bf728d9c12bb7969a49003d783d03020aec824f7
Gaetan518 <96253862+Gaetan518@users.noreply.github.com>
2022-05-05 11:42:26 +0200

    Update début_de_partie.py

--- début_de_partie.py
+++ début_de_partie.py
@@ -3,10 +3,10 @@ liste_mot = open("liste_mot.csv")
 t1 = list(csv.DictReader(liste_mot, delimiter = ";")) 
 
 import random
-def mot_hasard(t1): 
+def mot_hasard(t1):
+     """Tire un mot au hasard parmi la liste de mot"""
     mot = random.choice(t1)
     return mot
-mot_hasard(t1)
 
 def nombre_lettre(mot):
     """Compter le nombre de lettres du mot choisi"""
