91956fb0b026d68d6a7e5a9c5cefbb4a4637ca6f
Gaetan518 <96253862+Gaetan518@users.noreply.github.com>
2022-05-10 17:25:39 +0200

    Update début_de_partie.py

--- début_de_partie.py
+++ début_de_partie.py
@@ -4,7 +4,7 @@ t1 = list(csv.DictReader(liste_mot, delimiter = ";"))
 
 import random
 def mot_hasard(t1):
-     """Tire un mot au hasard parmi la liste de mot"""
+     """ Tire un mot au hasard parmi la liste de mot """
     mot = random.choice(t1)
     return mot
 
