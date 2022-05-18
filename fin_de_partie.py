4ae7a0150980007e2789af19a13350a5528af14d
Alchch1810 <96254377+Alchch1810@users.noreply.github.com>
2022-05-18 11:39:01 +0200

    Update fin_de_partie.py

--- fin_de_partie.py
+++ fin_de_partie.py
@@ -1,32 +1,68 @@
-def resultat(mot, lettre):
-    compteur = 6            #/!\ se sont des valeurs que nous devront implementer.
-    points = 0
+import csv
+""" Import du fichier csv qui contient la liste de mot """
+import random
+""" Import de la variable random pour tirer un mot au hasard dans la liste """
 
-def win(lettre, mot):
-    """ Defini si le joueur a placer toute les lettre du mot choisidans la bonne place et 
-        que le compteur ne soit pas = 0 , le joueur gagne et retourne un texte """
+
+def resultat(lettre, compteur, mot):
+    """Quand le joueur a trouvé le mot la fonction renvoi win, si le joueur 
+    n'a pas trouvé mais est = a 0, elle renvois loose,
+    si le joueur n'a pas trouvé mais le compteur n'est pas = a 0 il a encore des essais"""
     win = "Tu as gagné"
+    loose = "Tu as perdu"
     if lettre == mot:
-        return win
-    
-def lose(lettre, mots):
-    """ Defini si le joueur n'a pas placer toutes les lettre du mot dans la bonne place 
-        mais que le compteur est = 0 , le joueur perd et retourne un texte """
-    lose = "tu as perdue"
-    if compteur == 0:
-        return lose
-    
-def retry(lettre, mot):
-    """ Si le lettre du mot ne sont pas toute dans la bonne place mais que 
-        le compteur n'est pas a 0, le joueur a un nouvelle essai de deviner """
-    if lettre != mots:
-        compteur - 1
-    
-def point(compteur):
-    """ Quand le joueur a ganger, des points sont comptabiliser 
-        en fonction du nombre d'essai que le joueur a du utiliser """
-    points = 0
-    if compteur >= 1:
-        points = compteur
+        return True
+    else:
+        compteur == 0
+        return False
+    if lettre != mot:
+        compteur = compteur - 1
+        return compteur
+         
+def mot_hasard(t1):
+    """ Tire un mot au hasard dans la liste et défini sa variable """
+    string = ""
+    mot = random.choice(t1)
+    for elem in mot.values():
+        string += elem.lower()
+    return string
 
+def nombre_lettre(mot):
+    """ Défini le nombre de lettres du mot choisi en utilisant une boucle """
+    nb_lettre = 0
+    for lettre in len(mot):
+        nb_lettre += 1
+        print("Le mot choisi contient",nb_lettre,"lettres.")
+
+def verif_lettre_bon_place(let1, let2):
+    if ord(let1) == ord(let2):
+        return True
+
+def verif_lettre_mauvaise_place(let1, mot):
+    for let2 in mot:
+        if ord(let1) == ord(let2):
+            return True
+
+def verif_mot(mot,mot2):
+    """ Vérifie que mot2 correspond à la variable mot à deviner
+        mot: str proposée
+        mot2: str à deviner """
+
+    for i in range (len(mot)):
+        if verif_lettre_bon_place(mot[i], mot2[i]):
+            print("bonne place :" ,mot[i])
+        elif verif_lettre_mauvaise_place(mot[i], mot2):
+            print("mauvaise place :" ,mot[i])
+        else:
+            print("n'existe pas :" ,mot[i])
+            
+compteur = 6        
+if __name__ == "__main__":
+    liste_mot = open("liste_mot.csv") 
+    liste_de_mots = list(csv.DictReader(liste_mot, delimiter = ";"))
+    mot_a_deviner = mot_hasard(liste_de_mots)
+    phrase = "Mot de "+str(len(mot_a_deviner))+" caractères, entrez votre proposition : "
+    for i in range(compteur):
+        verif_mot(input(phrase), mot_a_deviner)
+        resultat(mot_a_deviner,mot_hasard,compteur)
         
