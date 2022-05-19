266d2f0c128e3784205fe8a19dcc6f9a539ec9d7
Gaetan518 <96253862+Gaetan518@users.noreply.github.com>
2022-05-19 11:17:49 +0200

    Update vérif_mot.py

--- vérif_mot.py
+++ vérif_mot.py
@@ -4,19 +4,19 @@ def verif_lettre_bon_place(let1, let2):
 
 def verif_lettre_mauvaise_place(let1, mot):
     for let2 in mot:
-        if ord(let1)==ord(let2):
+        if ord(let1) == ord(let2):
             return True
 
 def verif_mot(mot,mot2):
     """ Vérifie que mot2 correspond à la variable mot à deviner
-        mot: str à deviner
-        mot2: str proposée """
+        mot: str proposée
+        mot2: str à deviner """
 
-    nouv_varia = [0,1,1,0,1]
     for i in range (len(mot)):
         if verif_lettre_bon_place(mot[i], mot2[i]):
-            print("bonne place")
+            print("bonne place :" ,mot[i])
         elif verif_lettre_mauvaise_place(mot[i], mot2):
-            print("mauvaise place")
+            print("mauvaise place :" ,mot[i])
         else:
-            print("n'existe pas")
+            print("n'existe pas :" ,mot[i])
+            
